b127a23617c42a52140f683d315be7bbeafb2711
Balde <abdourahamane620@gmail.com>
2024-04-14 09:55:27 +0200

    Debut du projet

---
+++ src/main/java/com/ocprojettrois/locationprojettree/LocationprojettreeApplication.java
@@ -0,0 +1,13 @@
+package com.ocprojettrois.locationprojettree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationprojettreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationprojettreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ocprojettrois/locationprojettree/LocationprojettreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocprojettrois.locationprojettree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationprojettreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
