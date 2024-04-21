8f17ede3038d3aa52e35cefe804176a8ce623648
Balde <abdourahamane620@gmail.com>
2024-04-21 21:59:51 +0200

    Ajout d'un fichier .env et modification pour utiliser les variables d'environnement

--- src/main/java/com/ocprojettrois/locationprojettree/LocationprojettreeApplication.java
+++ src/main/java/com/ocprojettrois/locationprojettree/LocationprojettreeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LocationprojettreeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LocationprojettreeApplication.class, args);
 	}
